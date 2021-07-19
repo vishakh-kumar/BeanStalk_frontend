cc94f2c42b67e6446bfa0c5f8beb76a6f795ef0e
jacobhall <hall.j.alexander@gmail.com>
2021-07-19 18:34:25 -0400

    Adds full CRUD to Roasters and creates individual UI components that all render in App.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,18 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./App.css";
-import BusinessSignIn from "./components/BusinessSignIn";
+import RoasterSignUp from "./components/RoasterSignUp";
+import RoasterSignIn from "./components/RoasterSignIn";
+import RoasterList from "./components/RoasterList";
 
 function App() {
-    const [roastersState, setRoastersState] = useState({ roasters: [] });
+    const [signedInRoaster, setSignedInRoaster] = useState(null);
 
-    useEffect(() => {
-        async function getRoasters() {
-            try {
-                const roasters = await fetch("http://localhost:3000").then(
-                    (response) => response.json()
-                );
-                setRoastersState({ roasters });
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        getRoasters();
-    }, []);
-    async function handleAdd(formInputs) {
-        try {
-            const roasters = await fetch("http://localhost:3000", {
-                method: "POST",
-                header: {
-                    "Content-Type": "Application/json",
-                },
-                body: JSON.stringify(formInputs),
-            }).then((res) => res.json());
-            setRoastersState({ roasters });
-        } catch (error) {
-            console.log(error);
-        }
-    }
     return (
         <div className="App">
-            <BusinessSignIn handleAdd={handleAdd} />
+            <RoasterSignUp />
+            <RoasterSignIn signIn={(roaster) => setSignedInRoaster(roaster)} />
+            {signedInRoaster && <div>{signedInRoaster} test</div>}
+            <RoasterList />
         </div>
     );
 }
---
+++ src/components/RoasterList.js
@@ -0,0 +1,50 @@
+import {useEffect, useState} from "react";
+import RoasterUpdate from "./RoasterUpdate";
+import axios from "axios";
+
+export default function RoasterList() {
+    const [roasterList, setRoasterList] = useState(null);
+    const [displayRoaster, setDisplayRoaster] = useState(null);
+
+    useEffect(() => {
+        getRoasters();
+    }, [])
+
+    const getRoasters = async () => {
+        let axiosConfig = {
+            headers: {
+                "Content-Type": "application/json;char=UTF-8",
+                "Access-Control-Allow-Origin": "*",
+            },
+        };
+        try {
+            axios.get('http://localhost:3000/roasters', axiosConfig)
+                .then(res => {
+                    console.log(res)
+                    setRoasterList(res.data.roaster)
+                })
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    return (
+        <>
+            {roasterList && (
+                roasterList.map((roaster, idx) => {
+                    return <div
+                        key={roaster.id}
+                        onClick={() => {
+                            console.log(roasterList, idx, roasterList[idx])
+                            setDisplayRoaster(idx)
+                        }}
+                    >
+                        {roaster.email}
+                    </div>
+                })
+            )}
+
+            {displayRoaster !== null && <RoasterUpdate roaster={roasterList[displayRoaster]} />}
+        </>
+    )
+}
