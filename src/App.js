f97f1868a61efb7c7b93daec530af316d861d157
Vishakh Aswani Kumar <vishakh@Vishakhs-MacBook-Pro.local>
2021-07-20 15:06:35 -0500

    feat: photo upload

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import RoasterSignUp from "./components/RoasterSignUp";
 import RoasterSignIn from "./components/RoasterSignIn";
 import RoasterList from "./components/RoasterList";
+import PhotoUpload from "./components/PhotoUpload";
 
 function App() {
     const [signedInRoaster, setSignedInRoaster] = useState(null);
@@ -11,6 +12,7 @@ function App() {
         <div className="App">
             <RoasterSignUp />
             <RoasterSignIn signIn={(roaster) => setSignedInRoaster(roaster)} />
+            <PhotoUpload />
             {signedInRoaster && <div>{signedInRoaster} test</div>}
             <RoasterList />
         </div>
--- src/components/PhotoUpload.js
+++ src/components/PhotoUpload.js
@@ -1,17 +1,45 @@
 import React from "react";
-import { AdvancedImage } from "@cloudinary/react";
-import { Cloudinary } from "@cloudinary/base";
+import { useState } from "react";
 
 const PhotoUpload = () => {
-    const CLOUDINARY_URL = `https://api.cloudinary.com/v1_1/dz4ioqyl1/image/upload`;
-    const cld = new Cloudinary({
-        cloud: {
-            cloudName: "demo",
-        },
-    });
-    const myImage = cld.image("sample");
+    const [image, setImage] = useState(null);
+    const [imageString, setImageString] = useState(null);
+    const onChange = (e) => {
+        console.log(e);
+        e.persist();
+        setImage(e.target.files[0]);
+    };
 
-    return <div></div>;
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        const form = new FormData();
+        form.append("image", image);
+
+        await fetch(`http://localhost:3000/photos`, {
+            header: {
+                "Content-Type": "application/json;char=UTF-8",
+                "Access-Control-Allow-Origin": "*",
+            },
+            method: "POST",
+            body: form,
+        }).then((res) => {
+            console.log(res.json());
+            setImageString(res.json().secure_url);
+        });
+    };
+
+    return (
+        <div className="form">
+            <h1>Upload Image</h1>
+            <form onSubmit={onSubmit}>
+                <label>Image Upload</label>
+                <input type="file" name="image" onChange={onChange} />
+                <br />
+                <input type="submit" />
+            </form>
+            {imageString && <img src={imageString}></img>}
+        </div>
+    );
 };
 
 export default PhotoUpload;
