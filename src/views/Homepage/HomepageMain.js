d96a03f26048b3475f1c2b5723c6cf364db23ab6
jacobhall <hall.j.alexander@gmail.com>
2021-07-20 14:51:07 -0400

    Adds Paper Kit UI

---
+++ src/views/Homepage/HomepageMain.js
@@ -0,0 +1,67 @@
+import React from "react";
+
+// reactstrap components
+import { Button, Container, Row, Col } from "reactstrap";
+
+// core components
+
+function HomepageMain() {
+  let pageHeader = React.createRef();
+
+  React.useEffect(() => {
+    if (window.innerWidth > 991) {
+      const updateScroll = () => {
+        let windowScrollTop = window.pageYOffset / 3;
+        pageHeader.current.style.transform =
+          "translate3d(0," + windowScrollTop + "px,0)";
+      };
+      window.addEventListener("scroll", updateScroll);
+      return function cleanup() {
+        window.removeEventListener("scroll", updateScroll);
+      };
+    }
+  });
+
+  return (
+    <>
+      <div
+        className="page-header"
+        ref={pageHeader}
+        style={{
+          backgroundImage:
+            "url(" +
+            require("assets/img/coffeeBeans.jpg").default +
+            ")",
+        }}
+      >
+        <div className="filter" />
+        <div className="content-center">
+          <Container>
+            <div className="title-brand">
+              <h1 className="presentation-title">Life is but a bean</h1>
+              <h3 className="description">
+                Discover and review unique coffee roasters
+              </h3>
+              <br />
+              <Row>
+                <Col md={{ size: 4, order: 2, offset: 4 }} >
+                  <Button
+                    className="btn-round btn-move-right mr-1"
+                    block
+                    color="danger"
+                    href="https://www.youtube.com/watch?v=dQw4w9WgXcQ?ref=creativetim"
+                    target="_blank"
+                  >
+                    Get Started <i className="nc-icon nc-minimal-right" />
+                  </Button>
+                </Col>
+              </Row>
+            </div>
+          </Container>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default HomepageMain;
