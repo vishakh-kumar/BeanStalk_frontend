fa52f0cb5a37202e5d4ae194f9cff79b0011e9a5
Vishakh Aswani Kumar <vishakh@Vishakhs-MacBook-Pro.local>
2021-07-22 15:21:10 -0500

    wip: roast display

--- src/components/RoasterList.js
+++ src/components/RoasterList.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import RoasterUpdate from "./RoasterUpdate";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default function RoasterList() {
     const [roasterList, setRoasterList] = useState(null);
@@ -8,44 +9,51 @@ export default function RoasterList() {
 
     useEffect(() => {
         getRoasters();
-    }, [])
+    }, []);
     // Need to fix the access control origin
     const getRoasters = async () => {
         let axiosConfig = {
             headers: {
                 "Content-Type": "application/json;char=UTF-8",
                 "Access-Control-Allow-Origin": `${process.env.REACT_APP_BACKEND_URL}`,
-                "withCredentials": "true"
+                withCredentials: "true",
             },
         };
         try {
-            axios.get(`${process.env.REACT_APP_BACKEND_URL}/roasters`, axiosConfig)
-                .then(res => {
-                    console.log(res)
-                    setRoasterList(res.data.roaster)
-                })
+            await axios
+                .get(
+                    `${process.env.REACT_APP_BACKEND_URL}/roasters`,
+                    axiosConfig
+                )
+                .then((res) => {
+                    console.log(res);
+                    setRoasterList(res.data.roaster);
+                });
         } catch (e) {
-            console.log(e)
+            console.log(e);
         }
-    }
+    };
 
     return (
         <>
-            {roasterList && (
+            {roasterList &&
                 roasterList.map((roaster, idx) => {
-                    return <div
-                        key={roaster.id}
-                        onClick={() => {
-                            console.log(roasterList, idx, roasterList[idx])
-                            setDisplayRoaster(idx)
-                        }}
-                    >
-                        {roaster.email}
-                    </div>
-                })
-            )}
+                    return (
+                        <div
+                            key={roaster.id}
+                            onClick={() => {
+                                console.log(roasterList, idx, roasterList[idx]);
+                                setDisplayRoaster(idx);
+                            }}
+                        >
+                            {roaster.name}
+                        </div>
+                    );
+                })}
 
-            {displayRoaster !== null && <RoasterUpdate roaster={roasterList[displayRoaster]} />}
+            {displayRoaster !== null && (
+                <RoasterUpdate roaster={roasterList[displayRoaster]} />
+            )}
         </>
-    )
+    );
 }
